d3cb8b41e86c8e66980c88fde51dfad29dad111e
PicuBicu <championello@gmail.com>
2022-01-29 20:33:11 +0100

    Add cam settings

---
+++ camsettings.hpp
@@ -0,0 +1,9 @@
+// Edit this file accordingly to your local computer setup
+#pragma once
+
+#define LOCAL_CAMERA_WEBCAM 0
+#define LOCAL_CAMERA_IRIUN 1
+#define LOCAL_VIDEO_RECORDING "converted_nowe_karteczki.mp4"
+
+// Edit this value to switch between predefined cameras
+#define SELECTED_CAMERA LOCAL_CAMERA_WEBCAM
